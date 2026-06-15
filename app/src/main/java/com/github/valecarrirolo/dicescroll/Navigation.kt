7b93235fcf68a0afb3ed802e919b4282996613a5
valecarrirolo <valentina.carrirolo@gmail.com>
2026-06-15 18:41:27 +0200

    refactor: optimize NavigationKeys by moving Main NavKey into Navigation.kt

--- app/src/main/java/com/github/valecarrirolo/dicescroll/Navigation.kt
+++ app/src/main/java/com/github/valecarrirolo/dicescroll/Navigation.kt
@@ -1,18 +1,16 @@
-@file:OptIn(
-  androidx.compose.material3.ExperimentalMaterial3Api::class,
-  androidx.compose.foundation.ExperimentalFoundationApi::class,
-  androidx.compose.foundation.layout.ExperimentalLayoutApi::class,
-)
-
 package com.github.valecarrirolo.dicescroll
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavKey
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberNavBackStack
 import androidx.navigation3.ui.NavDisplay
 import com.github.valecarrirolo.dicescroll.ui.main.MainScreen
+import kotlinx.serialization.Serializable
+
+@Serializable data object Main : NavKey
 
 @Composable
 fun MainNavigation() {
--- app/src/main/java/com/github/valecarrirolo/dicescroll/NavigationKeys.kt
+++
@@ -1,6 +0,0 @@
-package com.github.valecarrirolo.dicescroll
-
-import androidx.navigation3.runtime.NavKey
-import kotlinx.serialization.Serializable
-
-@Serializable data object Main : NavKey
